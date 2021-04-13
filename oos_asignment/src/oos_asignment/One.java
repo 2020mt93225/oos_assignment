593e68de30ff31dc738131c0bbcd4adb4a870040
user <user@user-PC>
2021-04-13 16:02:38 +0530

    FIrst change commit by contributor 1

--- oos_asignment/src/oos_asignment/One.java
+++ oos_asignment/src/oos_asignment/One.java
@@ -1,10 +1,11 @@
 package oos_asignment;
 
-public class One {
+public class One extends Two {
 
 	public static void main(String[] args) {
 		System.out.println("Test_one");
-
+name();
 	}
 
+	
 }
--- oos_asignment/src/oos_asignment/Two.java
+++ oos_asignment/src/oos_asignment/Two.java
@@ -2,4 +2,8 @@ package oos_asignment;
 
 public class Two {
 
+public static void name() {
+	System.out.println("Test_two");
+}
+
 }
